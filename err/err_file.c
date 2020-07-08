66da50f051bf3190d5b766b60833fa1d7338a737
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-07-08 15:42:47 +0800

    打开文件错误

---
+++ err/err_file.c
@@ -0,0 +1,24 @@
+#include <stdio.h> /*声明perror()*/
+#include <errno.h> /*定义错误代码errno*/
+#include <string.h> /*声明strerror()*/
+
+extern int errno;
+
+int main(void)
+{
+	FILE *fp;
+	int errnum;
+
+	fp = fopen("a.txt", "rb");
+	if (fp == NULL) {
+		errnum = errno;
+		fprintf(stderr, "错误代码：%d\n", errno);
+		perror("Print the error case");
+		fprintf(stderr, "错误原因：%s\n", strerror(errnum));
+	} else {
+		fclose(fp);
+	}
+
+	return 0;
+}
+
