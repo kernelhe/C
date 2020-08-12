3bf6554ac1e4b3f3ac1ebbf8577af2d0a6461c02
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-08-12 23:45:19 +0800

    modify code

--- code/temp/const.c
+++ code/temp/const.c
@@ -6,10 +6,12 @@ int main(void)
 	const int WIDTH = 5;
 	const char NEWLINE = '\n';
 	int area;
+	const int num[10] = {};
 
 	area = LENGTH * WIDTH;
 	printf("value of area:%d", area);
 	printf("%c", NEWLINE);
+	printf("%d", num[0]);
 
 	return 0;
 }
