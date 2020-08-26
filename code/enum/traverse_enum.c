ba136376d0de6e256d5a31ab5a4c157fdc581fcb
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-08-26 16:56:02 +0800

    add traverse_enum.c

--- code/enum/traverse_enum.c
+++ code/enum/traverse_enum.c
@@ -16,11 +16,34 @@ int main(void)
 	printf("current enum number\n");
 	
 	for (day = MON; day <= SUN; day++) {
-		switch (day & MON | TUE | WED | THU | FRI | SAT | SUN) {
+		switch ((day & MON) | (day & TUE) | (day & WED) | 	\
+			(day & THU) | (day & FRI) | (day & SAT) | (day & SUN)) {
 		case 1:
-
+			printf("MON:%d\n", day);
+			break;
+		case 2:
+			printf("TUE:%d\n", day);
+			break;
+		case 3:
+			printf("WED:%d\n", day);
+			break;
+		case 4:
+			printf("THU:%d\n", day);
+			break;
+		case 5:
+			printf("FRI:%d\n", day);
+			break;
+		case 6:
+			printf("SAT:%d\n", day);
+			break;
+		case 7:
+			printf("SUN:%d\n", day);
+			break;
+		default:
+			printf("error\n");//default的break语句不是必须的
 		}
 	}
+
 	return 0;
 }
 
