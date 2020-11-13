bfc60d42ca63af55275a7f05e7a6cb8d28d4f7b4
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-11-13 23:01:46 +0800

    add new code

--- code/force-type-shift/type_shift.c
+++ code/force-type-shift/type_shift.c
@@ -1,13 +1,39 @@
 /*
  * 强制类型转换的作用：
- * 	如果一个运算符两边 
+ * 	如果一个运算符两边的运算类型不同，先要将其转换为相同的类型
+ * 	即较低类型转换为较高类型，然后参加运算，转换规则如下：
+ *    long double	高
+ * 	  ^
+ * 	  |
+ * 	double		
+ * 	  ^
+ * 	  |
+ * 	float
+ * 	  ^
+ * 	  |
+ * unsigned long long
+ * 	  ^
+ * 	  |
+ * 	long long
+ * 	  ^
+ * 	  |
+ *    unsigned long
+ * 	  ^
+ * 	  |
+ * 	long
+ * 	  ^
+ * 	  |
+ *    unsigned int
+ * 	  ^
+ * 	  |
+ * 	 int	<-----	char ,short  低
  */
 
 #include <stdio.h>
 
 int main(void)
 {
-	int value = 100;
+	int value = 1
 	int data = 55;
 
 	double result;
