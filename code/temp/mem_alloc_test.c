e2a40cbad5053d0d34a072aa127495213d7c8021
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-11-30 16:27:05 +0800

    modify code

--- code/temp/mem_alloc_test.c
+++ code/temp/mem_alloc_test.c
@@ -6,15 +6,15 @@
 
 #define SIZE_MAX(n)	pow(1024.0, 3.0)*n
 
-#if SIZE_64B
+#if SIZE_64B				/* alloc from Stack */
  #define SIZE_BLOCK	64UL 
-#elif SIZE_1K
+#elif SIZE_1K				/* alloc from Heap  */
  #define SIZE_BLOCK	1024UL
-#elif SIZE_4K
+#elif SIZE_4K				/* alloc from Heap  */
  #define SIZE_BLOCK	4096UL
-#elif SIZE_64K
+#elif SIZE_64K				/* alloc from Heap  */
  #define SIZE_BLOCK	65536UL
-#elif SIZE_4M
+#elif SIZE_4M				/* alloc from Heap  */
  #define SIZE_BLOCK	4194304UL
 #endif
 
@@ -40,43 +40,59 @@ int main(void)
 	default:
 		total = (unsigned long)(SIZE_MAX(2) / SIZE_BLOCK);
 	}
-	printf("Total memory you selected: %dG\n", size);
+
+	int times;
+	printf("Please set cycle times...\n");
+	scanf("%d", &times);
+
+	printf("Total memory you selected: %-dG\n", size);
 #if SIZE_64B
-	printf("Block   size you selected: 64B\n");
+	printf("Block size   you selected: 64B\n");
 #elif SIZE_1K
-	printf("Block   size you selected: 1K\n");
+	printf("Block size   you selected: 1K\n");
 #elif SIZE_4K
-	printf("Block   size you selected: 4K\n");
+	printf("Block size   you selected: 4K\n");
 #elif SIZE_64K
-	printf("Block   size you selected: 64K\n");
+	printf("Block size   you selected: 64K\n");
 #elif SIZE_4M
-	printf("Block   size you selected: 4M\n");
+	printf("Block size   you selected: 4M\n");
 #endif
+	printf("Cycle times  you selected: %-d\n", times);
 	printf("==================================================\n");
 	sleep(2);
 	printf("OK, Start allocating memory...\n");
-	printf("total: %llu\n", total);
 	
-	char block[SIZE_BLOCK];
-	char * buffer[total];
-	unsigned long long num = 0;
-	
-	for (unsigned long long i = 0; i < total; i++) {
-		char *buf = (char *)malloc(sizeof(block));
-		if (!buf)
-			exit(1);
+	while (times--) {
+		/*
+		 * If you select SIZE_64B as block size, it will report a Segment fault.
+		 * So, we should control cycles, avoid stack overflow.
+		 * You could use command: ulimit -s, to check stack size in your machine.
+		 * In my machine ,it is 8M.
+		 */
+#if SIZE_64B
+		total = (unsigned long long)(pow(1024.0, 2.0) * 8) / 64;
+#endif
+		char block[SIZE_BLOCK];
+		char * buffer[total];
+		unsigned long long num = 0;
+		
+		for (unsigned long long i = 0; i < total; i++) {
+			char *buf = (char *)malloc(sizeof(block));
+			if (!buf)
+				exit(1);
 
-		buffer[i] = buf;
+			buffer[i] = buf;
+		}
+	
+		for (unsigned long long j = 0; j < total; j++) {
+			memset(buffer[j], num++, SIZE_BLOCK);
+			/* printf("buffer[%llu]:%llu\n", j, buffer[j][0]); 		*/
+			/* printf("buffer[%llu]:%llu\n", j, buffer[j][SIZE_BLOCK - 1]); */
+		} 
+
+		for (unsigned long long k = 0; k < total; k++)
+			free(buffer[k]);
 	}
-	
-	for (unsigned long long j = 0; j < total; j++) {
-		memset(buffer[j], num++, SIZE_BLOCK);
-		/* printf("buffer[%llu]:%llu\n", j, buffer[j][0]); 		*/
-		/* printf("buffer[%llu]:%llu\n", j, buffer[j][SIZE_BLOCK - 1]); */
-	} 
-
-	for (unsigned long long k = 0; k < total; k++)
-		free(buffer[k]);
 		
 	return 0;
 }
