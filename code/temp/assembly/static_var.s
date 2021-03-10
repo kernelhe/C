27ae80d19bcc438d8e81e0979abf0940d6379ba0
hdsFFFFFFFF <hdsFFFFFF@163.com>
2021-03-10 12:58:55 +0800

    add some file

---
+++ code/temp/assembly/static_var.s
@@ -0,0 +1,78 @@
+	.file	"static_var.c"
+	.text
+	.local	j
+	.comm	j,4,4
+	.globl	func1
+	.type	func1, @function
+func1:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	i.2251(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, i.2251(%rip)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func1, .-func1
+	.globl	func2
+	.type	func2, @function
+func2:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, j(%rip)
+	movl	j(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, j(%rip)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	func2, .-func2
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	.cfi_offset 3, -24
+	movl	$0, %ebx
+	jmp	.L4
+.L5:
+	call	func1
+	call	func2
+	addl	$1, %ebx
+.L4:
+	cmpl	$9, %ebx
+	jle	.L5
+	movl	$0, %eax
+	popq	%rbx
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.local	i.2251
+	.comm	i.2251,4,4
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
